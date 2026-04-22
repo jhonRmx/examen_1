ffdbaa84eaead49c57db7a329172bfb8312923cd
MAX <151407631+maxJR98@users.noreply.github.com>
2026-04-21 20:22:27 -0400

    Se crea la base de datos y la tabla pacientes

---
+++ init_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conexion = sqlite3.connect('citas.db')
+
+cursor = conexion.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS pacientes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mascota TEXT NOT NULL,
+        propietario TEXT NOT NULL,
+        especie TEXT,
+        fecha TEXT NOT NULL
+    )
+''')
+
+conexion.commit()
+conexion.close()
+
+print("Base de datos 'citas.db' y tabla 'pacientes' creadas con éxito.")
